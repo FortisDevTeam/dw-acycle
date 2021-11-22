460ba088c1272d79756ebdb02456f1f281376373
Bhaskar Dutta <bhaskardutta2209@gmail.com>
2021-11-23 03:27:30 +0530

    modified the cron for updated contract

--- index.js
+++ index.js
@@ -55,33 +55,31 @@ cron.schedule(process.env.CRON, async function () {
         // Get stake cycle count
         stake_cycle_count = entry.rows[0].stake_cycle_count;
 
-        console.log("Auction cycle count: ", auction_cycle_count);
-        console.log("Stake cycle count: ", stake_cycle_count);
-
+        // STEP 1: Freeze the contract
         await setfreeze(auctions[i].auctionid, 3);
         await new Promise((resolve) =>
           setTimeout(resolve, process.env.WAIT_AUCTION_CYCLE)
         );
 
-        console.log("recycle auction with auction id ", auctions[i].auctionid);
+        // STEP 2: Call recyclebyaid action
         await recyclebyaid(auctions[i].auctionid);
+        await new Promise((resolve) =>
+          setTimeout(resolve, process.env.WAIT_LOOPSTAKE_CYCLE)
+        );
+
+        // STEP 3: Call loopstakes action
+        await loopstakes(auctions[i].auctionid, auction_cycle_count);
         await new Promise((resolve) =>
           setTimeout(resolve, process.env.WAIT_STAKE_CYCLE)
         );
 
-        console.log(
-          "stake giveout with auction id ",
-          auctions[i].auctionid,
-          " and cycle count ",
-          // auctions[i].stakecycle
-          auction_cycle_count
-        );
-        // await stakegiveout(auctions[i].auctionid, auctions[i].auctioncycle);
+        // STEP 4: Call stakegiveout action
         await stakegiveout(auctions[i].auctionid, auction_cycle_count);
         await new Promise((resolve) =>
           setTimeout(resolve, process.env.WAIT_OPEN)
         );
 
+        // STEP 5: Unfreeze
         await setfreeze(auctions[i].auctionid, 0);
 
         // auctions[i].auctioncycle += 1;
@@ -165,7 +163,34 @@ async function recyclebyaid(auction_id) {
   }
 }
 
-async function stakegiveout(auction_id, cycle_count) {
+async function loopstakes(auction_id, auction_cycle) {
+  const stakes = await getStakesCount();
+  const denominator = process.env.LOOPSTAKE_DENOMINATOR;
+  const quotient = Math.floor(stakes / denominator);
+  const remainder = stakes % denominator;
+
+  for (let i = 0; i < quotient; i++) {
+    await callLoopstakeAction(auction_id, auction_cycle, denominator);
+  }
+  await callLoopstakeAction(auction_id, auction_cycle, remainder);
+  console.log("loopstakes action COMPLETED");
+}
+
+async function stakegiveout(auction_id, auction_cycle) {
+  const count = await getTotalStakeCount();
+  const numberOfAcceptedTokens = await getAcceptedTokensCount(auction_id);
+  const denominator = process.env.STAKE_DENOMINATOR;
+  const quotient = Math.floor(count / denominator);
+  const remainder = count % denominator;
+  for (let i = 0; i < numberOfAcceptedTokens; i++) {
+    for (let j = 0; j < quotient; j++) {
+      await callStakegiveoutAction(auction_id, auction_cycle, denominator);
+    }
+    await callStakegiveoutAction(auction_id, auction_cycle, remainder);
+  }
+}
+
+async function callStakegiveoutAction(auction_id, auction_cycle, count) {
   try {
     const result = await api.transact(
       {
@@ -181,7 +206,47 @@ async function stakegiveout(auction_id, cycle_count) {
             ],
             data: {
               auction_id: auction_id,
-              auction_cycle: cycle_count,
+              auction_cycle: auction_cycle,
+              count: count,
+            },
+          },
+        ],
+      },
+      {
+        blocksBehind: 3,
+        expireSeconds: 30,
+      }
+    );
+    console.log(
+      "stakegiveout SUCCESS with auction_id = ",
+      auction_id,
+      " count: ",
+      count
+    );
+  } catch (exp) {
+    console.log("stakegiveout ran into error");
+    console.log(exp);
+  }
+}
+
+async function callLoopstakeAction(auction_id, auction_cycle, count) {
+  try {
+    const result = await api.transact(
+      {
+        actions: [
+          {
+            account: process.env.CONTRACT_NAME,
+            name: "loopstakes",
+            authorization: [
+              {
+                actor: process.env.ACTOR_NAME,
+                permission: process.env.PERMISSION,
+              },
+            ],
+            data: {
+              auction_id: auction_id,
+              auction_cycle: auction_cycle,
+              count: count,
             },
           },
         ],
@@ -193,13 +258,62 @@ async function stakegiveout(auction_id, cycle_count) {
     );
     //   console.dir(result);
     console.log(
-      "stakegiveout SUCCESS with auction_id = ",
+      "loopstakes SUCCESS for auction_id = ",
       auction_id,
-      " and cycle count ",
-      cycle_count
+      " count: ",
+      count
     );
   } catch (exp) {
-    console.log("stakegiveout ran into error");
+    console.log("callLoopstakeAction ran into error");
     console.log(exp);
   }
 }
+
+async function getStakesCount() {
+  const res = await rpc.get_table_rows({
+    json: true,
+    code: process.env.CONTRACT_NAME,
+    scope: process.env.CONTRACT_NAME,
+    table: "stakes",
+    limit: 10000000000000,
+  });
+  return res.rows.length;
+}
+
+async function getTotalStakeCount() {
+  const res = await rpc.get_table_rows({
+    json: true,
+    code: process.env.CONTRACT_NAME,
+    scope: process.env.CONTRACT_NAME,
+    table: "totalstake",
+    limit: 10000000000000,
+  });
+  return res.rows.length;
+}
+
+async function getAcceptedTokensCount(auction_id) {
+  const table = await rpc.get_table_rows({
+    json: true,
+    code: process.env.CONTRACT_NAME,
+    scope: process.env.CONTRACT_NAME,
+    table: "acceptedtkns",
+    limit: 10000000000000,
+  });
+  const data = table.rows.filter((row) => row.auction_id == auction_id);
+  return data.length;
+}
+
+/*
+
+TODO:
+
+1. Freeze (as usual)
+2. Get the total number of stakes
+3. Loop through all the stakes
+4. Get the total number of entries in totalstakes table
+5. Calculate and call stakegiveout
+6. Unfreeze (as usual)
+
+
+
+*/
